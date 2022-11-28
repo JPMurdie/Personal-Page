b3234d64222f5a7cf42eccae28fff7e3595bef20
Jason Murdie <jpmurdie@gmail.com>
2022-11-28 13:01:13 +0000

    commit: readme changed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+//import Styles
+import "./styles/app.scss";
+//import Components
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Nav />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav className="nav-container">
+      <p>Test Text</p>
+    </nav>
+  );
+};
+
+export default Nav;
